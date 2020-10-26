c7731d0ce913c6f3c4b01d9abf8f162891b98022
searcas <ultimatez1000@gmail.com>
2020-10-26 20:21:16 +0100

    Fixed Decent moving

--- Fraples7DevDX3D/Source/Fraples7DevDX3D/RendererAPI/Camera.cpp
+++ Fraples7DevDX3D/Source/Fraples7DevDX3D/RendererAPI/Camera.cpp
@@ -13,7 +13,7 @@ namespace FraplesDev
 			ImGui::SliderFloat("Y", &pos.y, -80.0f, 80.0f, "%.1f");
 			ImGui::SliderFloat("Z", &pos.z, -80.0f, 80.0f, "%.1f");
 			ImGui::Text("Orientation");
-			ImGui::SliderAngle("Pitch", &pitch, -90, 90.0f);
+			ImGui::SliderAngle("Pitch", &pitch, 0.955f * -90.0f, 0.955f * 90.0f);
 			if (ImGui::Button("Reset"))
 			{
 				Reset();
@@ -55,7 +55,7 @@ namespace FraplesDev
 	void Camera::Rotate(float dx, float dy) noexcept
 	{
 		yaw = wrap_angle(yaw + dx * rotationSpeed);
-		pitch = std::clamp(pitch + dy * rotationSpeed, -PI / 2.0f, PI / 2.0f);
+		pitch = std::clamp(pitch + dy * rotationSpeed, 0.955f * -PI / 2.0f, 0.955f * PI / 2.0f);
 
 	}
 	void Camera::Translate(DirectX::XMFLOAT3 translation) noexcept
