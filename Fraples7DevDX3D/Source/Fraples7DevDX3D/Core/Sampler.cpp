c07ecc27b4d6ac785569b81dbb5e340385b7b86d
searcas <ultimatez1000@gmail.com>
2020-11-03 11:01:40 +0100

    And Anisotropic support, you know just good stufff

--- Fraples7DevDX3D/Source/Fraples7DevDX3D/Core/Sampler.cpp
+++ Fraples7DevDX3D/Source/Fraples7DevDX3D/Core/Sampler.cpp
@@ -8,14 +8,14 @@ namespace FraplesDev
 	{
 		INFOMAN(gfx);
 		D3D11_SAMPLER_DESC samplerDesc = {};
-		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
 		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 		samplerDesc.MipLODBias = 0.0f;
 		samplerDesc.MinLOD = 0.0f;
 		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
-		
+		samplerDesc.MaxAnisotropy = D3D11_REQ_MAXANISOTROPY;
 		FPL_GFX_THROW_INFO(GetDevice(gfx)->CreateSamplerState(&samplerDesc, &pSampler));
 	}
 	void Sampler::Bind(Graphics& gfx) noexcept
