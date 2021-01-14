44c77535c31a4c7a594f1eae6e8cdc30d25f41e3
searcas <ultimatez1000@gmail.com>
2021-01-14 19:20:47 +0100

    Default imgui.ini

--- Fraples7DevDX3D/Source/Fraples7DevDX3D/ImGuiMgr/ImGuiMgr.cpp
+++ Fraples7DevDX3D/Source/Fraples7DevDX3D/ImGuiMgr/ImGuiMgr.cpp
@@ -1,11 +1,15 @@
 #include "ImGuiMgr.h"
 #include "imgui.h"
-
+#include <filesystem>
 namespace FraplesDev
 {
 
 	ImGuiMgr::ImGuiMgr()
 	{
+		if (!std::filesystem::exists("imgui.ini") && std::filesystem::exists("imgui_default.ini"))
+		{
+			std::filesystem::copy_file("imgui_default.ini", "imgui.ini");
+		}
 		IMGUI_CHECKVERSION();
 
 		ImGui::CreateContext();
