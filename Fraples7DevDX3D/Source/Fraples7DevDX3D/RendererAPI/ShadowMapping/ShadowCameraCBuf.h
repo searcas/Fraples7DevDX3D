e65fd2617b40250186aada3a3f049c35a7a9f90f
searcas <ultimatez1000@gmail.com>
2021-01-21 19:32:32 +0100

    PCF Hardware

--- Fraples7DevDX3D/Source/Fraples7DevDX3D/RendererAPI/ShadowMapping/ShadowCameraCBuf.h
+++ Fraples7DevDX3D/Source/Fraples7DevDX3D/RendererAPI/ShadowMapping/ShadowCameraCBuf.h
@@ -13,6 +13,7 @@ namespace FraplesDev
 		{
 			DirectX::XMMATRIX VieWProj;
 		};
+
 		ShadowCameraCBuf(Graphics& gfx, UINT slot = 1u);
 		void Bind(Graphics& gfx)noexcept(!IS_DEBUG)override;
 		void Update(Graphics& gfx);
--- Fraples7DevDX3D/Source/Fraples7DevDX3D/RendererAPI/ShadowMapping/ShadowSampler.cpp
+++ Fraples7DevDX3D/Source/Fraples7DevDX3D/RendererAPI/ShadowMapping/ShadowSampler.cpp
@@ -10,9 +10,10 @@ namespace FraplesDev
 
 		D3D11_SAMPLER_DESC samplerDesc = CD3D11_SAMPLER_DESC{ CD3D11_DEFAULT{} };
 		samplerDesc.BorderColor[0] = 1.0f;
+		samplerDesc.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_MIP_LINEAR;
 		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
 		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
-		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.ComparisonFunc = D3D11_COMPARISON_LESS_EQUAL;
 		FPL_GFX_THROW_INFO(GetDevice(gfx)->CreateSamplerState(&samplerDesc, &_mSampler));
 	}
 	void ShadowSampler::Bind(Graphics& gfx) noexcept(!IS_DEBUG)
